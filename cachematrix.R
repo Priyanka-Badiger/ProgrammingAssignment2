3bfa8a8d7d4e01ec678e1cc107aaeca0deff3dbb
Priyanka-Badiger <78835227+Priyanka-Badiger@users.noreply.github.com>
2021-06-07 11:40:32 +0530

    Week2assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These two functions help get inverse of a sqare matrix in cache environment
 
-## Write a short comment describing this function
-
+##This function gives a list, that is later used by cacheSolve 
 makeCacheMatrix <- function(x = matrix()) {
-
+     cache <- NULL
+     set <- function(y) {
+                x <<- y
+                cache <<- NULL
+        }
+     get <- function() x
+     setMatrix <- function(inverse) cache <<- inverse
+     getInverse <- function() cache
+     list(set = set, get = get,
+             setMatrix = setMatrix,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+ ## Returns a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       cache <- x$getInverse()
+       if (!is.null(cache)) {
+                message("getting cached data")
+               return(cache)
+        }
+       matrix <- x$get()
+       tryCatch( {
+                cache <- solve(matrix, ...)
+        },
+        error = function(e) {
+                message("Error:")
+                message(e)
+
+                return(NA)
+        },
+        warning = function(e) {
+                message("Warning:")
+                message(e)
+
+                return(NA)
+        },
+        finally = {
+                x$setMatrix(cache)
+        } )
+        return (cache)
 }
